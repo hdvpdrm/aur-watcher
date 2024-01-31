f0a5b96095a5e4f3d36685d5243f37f983f46b42
MAGANER <maganer.krik@mail.ru>
2024-01-31 04:42:46 +0300

    this module provides ability to scroll through aur-watcher output

---
+++ pager.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import sys
+import keyboard
+import os
+import signal
+
+def handler(signum, frame):
+    sys.exit(0)
+signal.signal(signal.SIGINT, handler)
+
+
+inp = sys.stdin.read().split("-"*40)
+
+current = 1
+total   = len(inp)
+
+os.system("clear")
+print("{}/{}".format(current,total))
+print(inp[current])
+r_pressed = l_pressed = False
+while True:
+    if keyboard.is_pressed('q'):
+        sys.exit(0)
+    elif keyboard.is_pressed('right') and not r_pressed:
+        if current + 1 >= total:
+            current = 1
+        else:
+            current+= 1
+        os.system("clear")
+        print("{}/{}".format(current,total))
+        print(inp[current])
+        r_pressed = True
+    elif keyboard.is_pressed('left') and not l_pressed:
+        if current - 1 <= 0:
+            current = total-1
+        else:
+            current-= 1
+        os.system("clear")
+        print("{}/{}".format(current,total))
+        print(inp[current])
+        l_pressed = True
+
+    if not keyboard.is_pressed("left"):
+        l_pressed = False
+    if not keyboard.is_pressed("right"):
+        r_pressed = False
+        
