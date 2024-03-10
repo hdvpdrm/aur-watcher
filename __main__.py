a8845429dcb71b4ddb8b3924d23900b1dcdc238f
MAGANER <maganer.krik@mail.ru>
2024-03-10 07:02:55 +0300

    aur-watcher is runnable package now

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import aurwatcher
+import sys
+import requests
+import json
+
+if __name__ == "__main__":
+    args = aurwatcher.parse_arguments()
+    response = requests.get(aurwatcher.compute_request(args))
+    if response.status_code != 200:
+        print("request failed with code {}".format(response.status_code))
+        sys.exit(1)
+
+
+    converted = dict(json.loads(response.text))["results"]
+    found_packages = [aurwatcher.extract_required_info(x,args["s"]) for x in converted]
+    aurwatcher.print_result(found_packages,True)
+    
