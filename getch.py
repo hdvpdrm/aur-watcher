62160675f50dd894d2826d2e559ba1694a217308
MAGANER <maganer.krik@mail.ru>
2024-03-10 07:35:59 +0300

    added function to read single char from user

---
+++ getch.py
@@ -0,0 +1,10 @@
+import sys, tty, termios
+def read_single_char():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
