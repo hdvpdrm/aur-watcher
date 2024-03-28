173fd6a26e773946d1dd7f9d605828c9e538fa99
MAGANER <maganer.krik@mail.ru>
2024-03-28 11:41:00 +0300

    fixed issues found with pylint

--- __main__.py
+++ __main__.py
@@ -1,18 +1,19 @@
-import aurwatcher
 import sys
-import requests
-import json
 import os
+import json
+
+import requests
+import aurwatcher
 
 if __name__ == "__main__":
     #read getch.py to see the reason
     if os.name != "posix":
         print("Sorry! This script won't run on non-posix system!")
         sys.exit(1)
-    
+
     args = aurwatcher.parse_arguments()
 
-    response = requests.get(aurwatcher.compute_request(args))
+    response = requests.get(aurwatcher.compute_request(args),timeout=5)
     if response.status_code != 200:
         print("request failed with code {}".format(response.status_code))
         sys.exit(1)
@@ -21,4 +22,3 @@ if __name__ == "__main__":
     converted = dict(json.loads(response.text))["results"]
     found_packages = [aurwatcher.extract_required_info(x,args["s"]) for x in converted]
     aurwatcher.print_result(found_packages,args["o"])
-    
--- aurwatcher.py
+++ aurwatcher.py
@@ -3,26 +3,38 @@
 
 import sys
 import argparse
-from functools import reduce
 import subprocess
 from getch import read_single_char
 
 def prepare_arguments():
-    parser = argparse.ArgumentParser(prog="aurwatcher",description="script looks for packages in AUR or in official repository.")
-    parser.add_argument("p",help="defines package-name substring to look for")
-    parser.add_argument("-s","--source",help="choose source of finding.",choices=["AUR","off"])
-    parser.add_argument("-o","--output",help="choose output mode(plain text xor inner pager)",choices=["pt","ip"])
+    '''create CLI parser'''
+    parser = argparse.ArgumentParser(prog="aurwatcher",
+                                     description="script looks for packages in AUR or in official repository.")
+    parser.add_argument("p",
+                        help="defines package-name substring to look for")
+    parser.add_argument("-s","--source",
+                        help="choose source of finding.",
+                        choices=["AUR","off"])
+    parser.add_argument("-o",
+                        "--output",
+                        help   ="choose output mode(plain text xor inner pager)",
+                        choices=["pt","ip"])
     return parser.parse_args()
 
 def parse_arguments():
+    '''This function is weird, I know, but it is required, because it's convert passed arguments to
+       old format i used before started to use argparse.
+
+       Note: i think it can be refactored.'''
     args = vars(prepare_arguments())
     result = {}
     result["o"] = "pt" if args["output"] is None else args["output"]
     result["s"] = "AUR" if args["source"] is None else args["source"]
     result["p"] =args["p"]
     return result
-    
+
 def compute_request(args):
+    '''generate rpc-request'''
     source = args["s"]
     package = args["p"]
 
@@ -37,24 +49,26 @@ def compute_request(args):
 
 
 def get_source_related_keys_list(source):
+    '''since response is big json file, then we need only specific keys'''
     if source == "aur":
         return ("Name","Description","Maintainer","URL","Version","OutOfData")
     else:
         return ("pkgname","pkgdesc","packager","repo","arch","url")
     
 def extract_required_info(response_result,source):
+    '''response is json-file and this function filters it to return only required data'''
     data = {}
     for key,value in response_result.items():
         if key in get_source_related_keys_list(source):
             data[key] = value
     return data
 
-
 def print_result(response, paging_mode=False):
+    '''print output as plain text or using "inner pager"'''
     paging_mode = True if paging_mode == "ip" else False
     
-    for id,item in enumerate(response):
-        print("found item #{}".format(id))
+    for i,item in enumerate(response):
+        print("found item #{}".format(i))
         print("-"*40)
         for k in item.keys():
             print("{}:{}".format(k,item[k]))
@@ -63,6 +77,6 @@ def print_result(response, paging_mode=False):
             if read_single_char() == 'q':
                 sys.exit(0)
             else:
-                subprocess.run(["clear"])
+                subprocess.run(["clear"]) #check isn't required here, because clear won't fail
 
     
