aee8b3c1d34400315128542a4e2c044650c2ab62
Hdvpdrm <hardcoderyan@gmail.com>
2024-09-28 21:10:32 +0300

    check whether it's arrows keys or other key

--- getch.py
+++ getch.py
@@ -1,12 +1,16 @@
 import sys, tty, termios
 
 
-def read_single_char():
+def read_single_char(fix_counter):
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
         ch = sys.stdin.read(1)
+        if str(ch) in "CABD":
+            fix_counter = True
+        else:
+            fix_counter = False
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+        return ch
